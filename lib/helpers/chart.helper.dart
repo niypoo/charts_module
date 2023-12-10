c4d58a4b209906c3fd98bb51555fec545bde39c5
Mahmoud Nabhan <niypoo@gmail.com>
2023-12-10 11:38:33 +0200

    test

--- lib/helpers/chart.helper.dart
+++ lib/helpers/chart.helper.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/widgets.dart';
 import 'package:fly_ui/extensions/responsive.extension.dart';
 import 'package:charts_module/models/chartData.model.dart';
+import 'package:get/get.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
 class ChartHelper {
   static RangeColumnSeries<ChartData, DateTime> barsRange({
     required List<ChartData>? chartData,
+    Function(ChartPointDetails)? onPointTap,
   }) {
     return RangeColumnSeries<ChartData, DateTime>(
-      selectionBehavior: SelectionBehavior(
-        enable: true,
-        selectedBorderWidth: 5,
-        unselectedBorderWidth: 0.5,
-      ),
+      // selectionBehavior: SelectionBehavior(
+      //   enable: true,
+      //   selectedColor: widget.selectedColor,
+      // ),
       // Legend
       name: chartData!.first.legendText,
       legendIconType: chartData.first.legendIconType ?? LegendIconType.circle,
@@ -36,10 +37,12 @@ class ChartHelper {
       highValueMapper: (ChartData data, _) => data.highValue,
       // Map the data label text for each point from the data source
       // isTrackVisible: false,
-      dataLabelSettings:const DataLabelSettings(
+      dataLabelMapper: (ChartData data, _) => '${data.value} ${data.label}',
+      dataLabelSettings: DataLabelSettings(
         isVisible: true,
+                textStyle: TextStyle(fontSize: 0, color: Get.theme.secondaryHeaderColor),
         useSeriesColor: true,
-        showZeroValue: false,
+        showZeroValue: true,
       ),
       // onPointTap: onPointTap,
     );
@@ -47,13 +50,13 @@ class ChartHelper {
 
   static SplineRangeAreaSeries<ChartData, DateTime> splineRange({
     required List<ChartData>? chartData,
+    Function(ChartPointDetails)? onPointTap,
   }) {
     return SplineRangeAreaSeries<ChartData, DateTime>(
-      selectionBehavior: SelectionBehavior(
-        enable: true,
-        selectedBorderWidth: 5,
-        unselectedBorderWidth: 0.5,
-      ),
+      // selectionBehavior: SelectionBehavior(
+      //   enable: true,
+      //   selectedColor: widget.selectedColor,
+      // ),
       // Legend
       name: chartData!.first.legendText,
       legendIconType: chartData.first.legendIconType ?? LegendIconType.circle,
@@ -76,29 +79,35 @@ class ChartHelper {
       xValueMapper: (ChartData data, _) => data.date,
       lowValueMapper: (ChartData data, _) => data.lowValue,
       highValueMapper: (ChartData data, _) => data.highValue,
-      dataLabelSettings: const DataLabelSettings(
+      // Map the data label text for each point from the data source
+      // isTrackVisible: false,
+      // dataLabelMapper: (ChartData data, _) => '${data.value} ${data.label}',
+      dataLabelSettings: DataLabelSettings(
         isVisible: true,
+                textStyle: TextStyle(fontSize: 0, color: Get.theme.secondaryHeaderColor),
         useSeriesColor: true,
-        showZeroValue: false,
+        showZeroValue: true,
         // labelAlignment: ChartDataLabelAlignment.middle,
         // alignment: ChartAlignment.center,
         // labelPosition: ChartDataLabelPosition.outside,
         // labelIntersectAction: LabelIntersectAction.hide,
         // showCumulativeValues: true,
       ),
+      onPointTap: onPointTap,
     );
   }
 
   static SplineSeries<ChartData, DateTime> spline({
     required List<ChartData> chartData,
+    Function(ChartPointDetails)? onPointTap,
   }) {
     return SplineSeries<ChartData, DateTime>(
       //MAKE SELECTED BAR WITH CUSTOM COLOR
-      selectionBehavior: SelectionBehavior(
-        enable: true,
-        selectedBorderWidth: 5,
-        unselectedBorderWidth: 0.5,
-      ),
+      // selectionBehavior: SelectionBehavior(
+      //   enable: true,
+      //   selectedBorderWidth: 5,
+      //   unselectedBorderWidth: 0.5,
+      // ),
 
       dataSource: chartData,
       width: 6,
@@ -121,14 +130,17 @@ class ChartHelper {
         width: 12,
         height: 12,
         shape: chartData.first.dataMarkerType,
+        // ignore: prefer_null_aware_operators
         color: chartData.first.color,
       ),
 
       // data label setting
-      dataLabelSettings: const DataLabelSettings(
+      dataLabelSettings: DataLabelSettings(
         isVisible: true,
+                textStyle: TextStyle(fontSize: 0, color: Get.theme.secondaryHeaderColor),
+
         useSeriesColor: true,
-        showZeroValue: false,
+        showZeroValue: true,
         // labelAlignment: ChartDataLabelAlignment.auto,
         // alignment: ChartAlignment.far,
         // labelPosition: ChartDataLabelPosition.inside,
@@ -136,19 +148,21 @@ class ChartHelper {
         // showCumulativeValues: false,
       ),
       splineType: SplineType.cardinal,
+      onPointTap: onPointTap,
     );
   }
 
   static ColumnSeries<ChartData, DateTime> bars({
     required List<ChartData> chartData,
+    Function(ChartPointDetails)? onPointTap,
   }) {
     return ColumnSeries<ChartData, DateTime>(
       //MAKE SELECTED BAR WITH CUSTOM COLOR
-      selectionBehavior: SelectionBehavior(
-        enable: true,
-        selectedBorderWidth: 5,
-        unselectedBorderWidth: 0.5,
-      ),
+      // selectionBehavior: SelectionBehavior(
+      //   enable: true,
+      //   selectedBorderWidth: 5,
+      //   unselectedBorderWidth: 0.5,
+      // ),
 
       dataSource: chartData,
       width: 6,
@@ -165,26 +179,29 @@ class ChartHelper {
       yValueMapper: (ChartData data, _) => data.value,
       dataLabelMapper: (ChartData data, _) => '${data.value} ${data.label}',
 
-      markerSettings: const MarkerSettings(
+      markerSettings: MarkerSettings(
         isVisible: true,
         borderWidth: 4,
         width: 12,
         height: 12,
-        // shape: chartData.first.dataMarkerType,
-        // color: chartData.first.color,
+        shape: chartData.first.dataMarkerType,
+        // ignore: prefer_null_aware_operators
+        color: chartData.first.color,
       ),
 
-      // // data label setting
-      dataLabelSettings: const DataLabelSettings(
+      // data label setting
+      dataLabelSettings: DataLabelSettings(
         isVisible: true,
+                textStyle: TextStyle(fontSize: 0, color: Get.theme.secondaryHeaderColor),
         useSeriesColor: true,
-        showZeroValue: false,
+        showZeroValue: true,
         // labelAlignment: ChartDataLabelAlignment.auto,
         // alignment: ChartAlignment.far,
         // labelPosition: ChartDataLabelPosition.inside,
         // labelIntersectAction: LabelIntersectAction.hide,
         // showCumulativeValues: false,
       ),
+      onPointTap: onPointTap,
     );
   }
 }
