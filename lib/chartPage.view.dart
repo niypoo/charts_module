3a6a65b91148ca66872237690244aa070a2dafa7
Mahmoud Nabhan <niypoo@gmail.com>
2023-12-11 11:57:51 +0200

    chart page view now need a chart as property

--- lib/chartPage.view.dart
+++ lib/chartPage.view.dart
@@ -1,26 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:charts_module/chart.widget.dart';
-import 'package:charts_module/enums/chartType.enum.dart';
-import 'package:charts_module/models/chartData.model.dart';
+
 import 'package:fly_ui/views/layouts/scaffoldLayout.widget.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
 
 class ChartPageView extends StatefulWidget {
   const ChartPageView({
     Key? key,
-    required this.charts,
-    this.horizontalPlotBands,
-    this.verticalPlotBands,
-    this.legendVisible = true,
-    this.chartType = ChartType.Spline,
+    required this.child,
+
   }) : super(key: key);
 
-  final List<PlotBand>? verticalPlotBands;
-  final List<PlotBand>? horizontalPlotBands;
-  final List<List<ChartData>> charts;
-  final bool legendVisible;
-  final ChartType chartType;
+  final Widget child;
 
   @override
   State<ChartPageView> createState() => _ChartPageViewState();
@@ -48,12 +38,7 @@ class _ChartPageViewState extends State<ChartPageView> {
   Widget build(BuildContext context) {
     return FlyScaffold.padding(
       appBar: AppBar(),
-      child: ChartWidget(
-        chartsData: widget.charts,
-        verticalPlotBands: widget.verticalPlotBands,
-        chartType: widget.chartType,
-        horizontalPlotBands: widget.horizontalPlotBands,
-      ),
+      child: widget.child,
     );
   }
 }
